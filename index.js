169bdf99a7e50e391b3686c76b1e95ceac76e07b
tychota <tychota@users.noreply.github.com>
2018-07-03 20:46:30 +0200

    :sparkles: (server) generate a basic hello world graphql endpoint

---
+++ index.js
@@ -0,0 +1,28 @@
+const { ApolloServer, gql } = require("apollo-server");
+
+// Type definitions define the "shape" of your data and specify
+// which ways the data can be fetched from the GraphQL server.
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// Resolvers define the technique for fetching the types in the
+// schema.  We'll retrieve books from the "books" array above.
+const resolvers = {
+  Query: {
+    hello: () => "world"
+  }
+};
+
+// In the most basic sense, the ApolloServer can be started
+// by passing type definitions (typeDefs) and the resolvers
+// responsible for fetching the data for those types.
+const server = new ApolloServer({ typeDefs, resolvers });
+
+// This `listen` method launches a web-server.  Existing apps
+// can utilize middleware options, which we'll discuss later.
+server.listen().then(({ url }) => {
+  console.log(`🚀  Server ready at ${url}`);
+});
